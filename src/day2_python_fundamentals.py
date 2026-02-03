04b3d08f3d50bcd6b22edb618f7d786e94185ba2
lady <firdous54088@gmail.com>
2026-02-03 14:42:02 +0530

    day2

--- src/day2_python_fundamentals.py
+++ src/day2_python_fundamentals.py
@@ -1,9 +1,13 @@
+# task 1
 name = input("Enter your name: ")
 age = int(input("Enter your current age: "))
 
 age_2030 = age+4
 
 print(f"Hey {name}, you will be {age_2030} years old in 2030.")
+
+
+# task 2
 total_bill = float(input("Enter total bill amount: "))
 people = int(input("Enter number of people: "))
 share = total_bill / people 
@@ -12,6 +16,9 @@ print(f"Each person pays: {share}")
 print(type(total_bill))
 print(type(people))
 print(type(share))
+
+
+# task 3
 item_name = "Laptop"
 quantity = 2 
 price = 499.99
